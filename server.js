bfacc8db468f7f620c3631d81c1be6746399dcfa
Timothy Rodriguez <tim.rdrgz2017@live.com>
2023-12-13 17:01:28 -0600

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -19,5 +19,17 @@ app.get("/greeting/:name", (req, res) => {
 app.get("/tip/:total/:tipPercentage", (req, res) => {
     let total = req.params.total 
     let tipPercentage = req.params.tipPercentage
+    // let tip = total * .20
     res.send(`${tipPercentage}`)
+})
+
+//*************************** MAGIC 8 BALL */
+
+app.get("/magic/:question", (req, res) => {
+    const magicQuestion = req.params.question
+    const magicEigthBallArray = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    let answer = magicEigthBallArray[Math.floor(Math.random() * magicEigthBallArray.length)]
+    res.send(`${magicQuestion}???
+    <h1>${answer}</h1>
+    `)
 })
